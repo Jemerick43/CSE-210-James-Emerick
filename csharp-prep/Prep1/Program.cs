01df0f1dad5e3d833b63d67f31ff812cef85efc8
James Emerick <jemerick43@byui.edu>
2023-09-16 09:23:56 -0600

    Completed both activities in week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string userFirstName = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string userLastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {userLastName}, {userFirstName} {userLastName}.");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage: ");
+        string userGPA = Console.ReadLine();
+        int gradePointAverage = int.Parse(userGPA);
+        string letter;
+
+        if (gradePointAverage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePointAverage >= 80 && gradePointAverage <= 90)
+        {
+            letter = "B";
+        }
+        else if (gradePointAverage >= 70 && gradePointAverage <= 80)
+        {
+            letter = "C";
+        }
+        else if (gradePointAverage >= 60 && gradePointAverage <= 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradePointAverage >= 70)
+        {
+            Console.WriteLine("You have passed the course! \n\nCongratulations!!\n\n");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course! \n\nDon't Give Up!! You can do this!\n\n");
+        }
     }
 }
