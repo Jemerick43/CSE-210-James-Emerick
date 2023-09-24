00dc6bd5b774653cf30f86c0630f1288107726fa
James Emerick <jemerick43@byui.edu>
2023-09-23 22:02:43 -0600

    Completed prep3 &4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Boolean running = true; //JE
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 10);
+        Console.WriteLine("Random Number Generated\n");
+
+        int numberOfGuesses = 0;
+
+        while (running)
+        {
+            Console.WriteLine("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine()); //JE
+            numberOfGuesses++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher ");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! It took you {0} guesses!", numberOfGuesses);
+            }
+
+            Console.WriteLine("Would you like to play again? (Y/N)");
+            string keepPlaying = Console.ReadLine(); //JE
+
+            if (keepPlaying == "y" || keepPlaying == "Y")
+            {
+                running = true; //JE
+            }
+            else
+            {
+                running = false; //JE
+            }
+
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+        Boolean running = true; //JE
+        Console.WriteLine("Please enter a series of numbers. Program will continue upon entry of '0': ");
+        while (running)
+        {
+            string number = Console.ReadLine();
+            if (number == "0")
+            {
+                running = false; //JE
+            }
+            else
+            {
+                numberList.Add(int.Parse(number)); //JE
+            }
+        }
+
+        int total = 0;  //JE
+        float avg = 0;        //JE
+        int max = 0;    //JE
+        foreach (int number in numberList)
+
+        {
+            total = number + total; //JE
+            avg = total / numberList.Count; //JE
+            if (number > max)
+            {
+                max = number; //JE
+            }
+        }
+
+        Console.WriteLine("The sum is: {0} \nThe average is: {1} \nThe largest number is: {2}", total, avg, max);
+
     }
 }
