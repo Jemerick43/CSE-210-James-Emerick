faaa186eca1792b2a8af21318871d7a72c354ce7
James Emerick <jemerick43@byui.edu>
2023-09-23 22:42:43 -0600

    finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        string userName = ""; //JE
+        Console.WriteLine("Please enter your name: ");
+        userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        int userNumber = 0;
+        Console.WriteLine("Please enter your favorite number: ");
+        userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int SquareNumber(int baseNumber)
+    {
+        int squaredNumber = baseNumber * baseNumber;
+        return squaredNumber;
+    }
+
+    static void DisplayResult(string userName, int squareNumber)
+    {
+        Console.WriteLine("{0}, the square of your number is {1}", userName, squareNumber);
     }
 }
