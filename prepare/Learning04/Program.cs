5b6e328a125b363bb1a071c848b775dda4f53f94
James Emerick <jemerick43@byui.edu>
2023-10-25 20:12:51 -0600

    Finished prep assignment (inheritance)

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Reflection.PortableExecutable;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new("Bro C", "CS210");
+        Console.WriteLine(myAssignment.GetSummary());
+        MathAssignment myMath = new("Roberto Rodriguez", "Fractions", "SectionCharacteristics 7.3", "Problems 8-19");
+        Console.WriteLine(myMath.GetSummary());
+        Console.WriteLine(myMath.GetHomeworkList());
+        WritingAssignment myWriting = new("Mary Waters", "European History", "The causes of WWII by Mary Waters");
+        Console.WriteLine(myWriting.GetSummary());
+        Console.WriteLine(myWriting.GetWritingInformation());
     }
 }
